ad9ff96f5baa53745715a2bea3f3c1d3480c2a86
maxroosters <github@maxroosters.com>
2026-08-02 22:19:33 +0000

    Change sidebar background color to #52525C

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(
 st.markdown("""
 <style>
 [data-testid="stSidebar"] {
-    background-color: #99A1AF;
+    background-color: #52525C;
 }
 </style>
 """, unsafe_allow_html=True)
